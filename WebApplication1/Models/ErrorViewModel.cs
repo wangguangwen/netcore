49a4d51b0ea8ac2398db7bb92d27f26def5cd77b
wangguangwen <349084496@qq.com>
2017-08-24 14:51:21 +0800

    ddd

--- WebApplication1/Models/ErrorViewModel.cs
+++ WebApplication1/Models/ErrorViewModel.cs
@@ -4,6 +4,9 @@ namespace WebApplication1.Models
 {
     public class ErrorViewModel
     {
+        /// <summary>
+        /// ddddd
+        /// </summary>
         public string RequestId { get; set; } 
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
