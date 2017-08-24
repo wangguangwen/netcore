8e8c1291cae3520868f2bcdf133934ae6a579163
wangguangwen <349084496@qq.com>
2017-08-24 14:47:50 +0800

    添加项目文件。

---
+++ WebApplication1/AppSetting.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    public class AppSetting
+    {
+        public testConfig testConfig { get; set; }
+    }
+    public class testConfig
+    {
+        public string key { get; set; }
+        public string value { get; set; }
+    }
+}
